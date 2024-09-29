3c92a066f64eb0d8ee141aaa15643673a1a4857b
AdamLunaAH <adam.a-h@outlook.com>
2024-09-29 16:07:15 +0200

    Change to top level statement

---
+++ MenuFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomat
+{
+    public static class MenuFunctions
+    {
+        public static void Pause()
+        {
+            Console.WriteLine("Tryck enter för att komma till menyn");
+            Console.ReadLine();
+        }
+    }
+}
